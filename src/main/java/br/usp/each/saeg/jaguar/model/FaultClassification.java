e5560e8b6f4abb8f1a38f0aebab85677c0478c19
Henrique Lemos Ribeiro <henriquelemos0@gmail.com>
2017-07-16 13:15:38 -0300

    Create fault classification class and tests

---
+++ src/main/java/br/usp/each/saeg/jaguar/model/FaultClassification.java
@@ -0,0 +1,108 @@
+package br.usp.each.saeg.jaguar.model;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This object represent the result of a Jaguar run.
+ * It contains all the requirements with their respective suspicious value.
+ *
+ * Created by henrique on 7/16/17.
+ */
+public class FaultClassification {
+
+    protected String project;
+    protected String heuristic;
+    protected Requirement.Type requirementType;
+    protected Long timeSpent;
+
+    /**
+     * Return a 'flat' list of {@link SuspiciousElement}.
+     * If it has a specific strucure, it will iterate over the subelements.
+     *
+     * @return A 'flat' list of {@link SuspiciousElement}
+     */
+    private List<Requirement> requirements = new ArrayList<Requirement>();
+
+    @XmlElement
+    public List<Requirement> getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(List<Requirement> requirements) {
+        this.requirements = requirements;
+    }
+
+    @XmlAttribute
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    @XmlAttribute
+    public String getHeuristic() {
+        return heuristic;
+    }
+
+    public void setHeuristic(String heuristic) {
+        this.heuristic = heuristic;
+    }
+
+    @XmlAttribute
+    public Requirement.Type getRequirementType() {
+        return requirementType;
+    }
+
+    public void setRequirementType(Requirement.Type requirementType) {
+        this.requirementType = requirementType;
+    }
+
+    @XmlAttribute
+    public Long getTimeSpent() {
+        return timeSpent;
+    }
+
+    public void setTimeSpent(Long timeSpent) {
+        this.timeSpent = timeSpent;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FaultClassification that = (FaultClassification) o;
+
+        if (project != null ? !project.equals(that.project) : that.project != null) return false;
+        if (heuristic != null ? !heuristic.equals(that.heuristic) : that.heuristic != null) return false;
+        if (requirementType != that.requirementType) return false;
+        if (timeSpent != null ? !timeSpent.equals(that.timeSpent) : that.timeSpent != null) return false;
+        return requirements != null ? requirements.equals(that.requirements) : that.requirements == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = project != null ? project.hashCode() : 0;
+        result = 31 * result + (heuristic != null ? heuristic.hashCode() : 0);
+        result = 31 * result + (requirementType != null ? requirementType.hashCode() : 0);
+        result = 31 * result + (timeSpent != null ? timeSpent.hashCode() : 0);
+        result = 31 * result + (requirements != null ? requirements.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "FaultClassification{" +
+                "project='" + project + '\'' +
+                ", heuristic='" + heuristic + '\'' +
+                ", requirementType=" + requirementType +
+                ", timeSpent=" + timeSpent +
+                ", requirements=" + requirements +
+                '}';
+    }
+}
---
+++ src/test/java/br/usp/each/saeg/jaguar/model/FautlClassificationTest.java
@@ -0,0 +1,64 @@
+package br.usp.each.saeg.jaguar.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by henrique on 7/16/17.
+ */
+public class FautlClassificationTest {
+
+    public void equals(){
+        FaultClassification fc1 = new FaultClassification();
+        FaultClassification fc2 = fc1;
+        Assert.assertTrue(fc1.equals(fc2));
+        Assert.assertEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc1 = new FaultClassification();
+        fc2 = new FaultClassification();
+        Assert.assertTrue(fc1.equals(fc2));
+        Assert.assertEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc1.setProject("project");
+        fc2.setProject("project");
+        Assert.assertTrue(fc1.equals(fc2));
+        Assert.assertEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc1.setHeuristic("heuristicType");
+        fc2.setHeuristic("heuristicType");
+        Assert.assertTrue(fc1.equals(fc2));
+        Assert.assertEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc1.setRequirementType(Requirement.Type.LINE);
+        fc2.setRequirementType(Requirement.Type.LINE);
+        Assert.assertTrue(fc1.equals(fc2));
+        Assert.assertEquals(fc1.hashCode(),fc2.hashCode());
+    }
+
+    @Test
+    public void notEquals(){
+        FaultClassification fc1 = new FaultClassification();
+        FaultClassification fc2 = new FaultClassification();
+
+        fc1.setProject("project");
+        Assert.assertFalse(fc1.equals(fc2));
+        Assert.assertNotEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc2.setProject("project");
+        fc1.setHeuristic("heuristicType");
+        Assert.assertFalse(fc1.equals(fc2));
+        Assert.assertNotEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc2.setHeuristic("heuristicType");
+        fc1.setRequirementType(Requirement.Type.LINE);
+        Assert.assertFalse(fc1.equals(fc2));
+        Assert.assertNotEquals(fc1.hashCode(),fc2.hashCode());
+
+        fc2.setRequirementType(Requirement.Type.DUA);
+        Assert.assertFalse(fc1.equals(fc2));
+        Assert.assertNotEquals(fc1.hashCode(),fc2.hashCode());
+    }
+}
